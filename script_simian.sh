670196e84265f98fce1d852707bb1a610b3220c9
Yuru Taustahuzau <yuru_taustahuzau@epam.com>
2014-05-05 17:09:56 +0300

    Fixing travis-ci build

--- script_simian.sh
+++ script_simian.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # =================     Download simian and unzip     ===========
-wget http://www.harukizaemon.com/simian/simian-2.3.34.tar.gz > /dev/null
-tar xzf simian-2.3.34.tar.gz > /dev/null
+wget http://www.harukizaemon.com/simian/simian-2.3.35.tar.gz > /dev/null
+tar xzf simian-2.3.35.tar.gz > /dev/null
 
 # =================     Run simian check     ===========
 ant check-simian
