f878e9ff3096d6ec8efe3f5ff7fbd55a4ecd5337
Anton Davydov <anton_davydov1@epam.com>
2015-01-28 14:50:35 +0300

    Formatting by code style.

--- libObjCAttr/Reflection/RFIvarInfo.h
+++ libObjCAttr/Reflection/RFIvarInfo.h
@@ -30,6 +30,7 @@
 //  See the NOTICE file and the LICENSE file distributed with this work
 //  for additional information regarding copyright ownership and licensing
 
+
 #import <Foundation/Foundation.h>
 
 
